8a061b37dc42eca6c8c4db1817b3c60196451a0e
helpthewalrus <alinatylets@gmail.com>
2018-09-28 23:23:20 +0300

    task_7_commit_1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let counter1 = 0;
+  let counter2 = 0;  
+  for (let i = number; i >= 1; i--) {
+    if (i % 5 === 0) {
+      let t = i;
+      while (t % 5 === 0) {
+        counter1++;
+        t = t / 5;
+      }
+    }
+    if (i % 2 === 0) {
+      let t = i;
+      while (t % 2 === 0) {
+        counter2++;
+        t = t / 2;
+      }
+    }
+  }
+  return (counter1 < counter2) ? counter1 : counter2;
 }
+
+//console.log(getZerosCount(15));
